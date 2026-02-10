78a2bbde615ef867a8bfed4d2a33158135a24a21
Venancio Carvalho <venanciocarvalho95@gmail.com>
2026-02-10 16:08:29 -0300

    refactor(Program.cs): A classe Program foi alterada para suportar o Channel.

--- Program.cs
+++ Program.cs
@@ -1,29 +1,59 @@
 ﻿using System;
-using nids.Sentinela.Engine;
 using SharpPcap;
+using System.Threading.Channels;
+using nids.Sentinela.Core;
+using nids.Sentinela.Engine;
 
 //Nome da placa de rede
 string nomeDaMinhaInterface = @"\Device\NPF_{EF3FDC02-A092-4E0C-A436-EA19432E74E0}";
 
-CaptureDeviceList devices = CaptureDeviceList.Instance;
-Analizador anl = new Analizador();
+// Configuração do Channel (O Tubo)
+// Capacidade de 50.000 pacotes. Se encher, descarta o mais novo para não travar a RAM.
+var channelOptions = new BoundedChannelOptions(50000)
+{
+    FullMode = BoundedChannelFullMode.DropWrite,
+    SingleWriter = true, // Só o PacketCaptureService escreve
+    SingleReader = true  // Só o AnalysisService lê
+};
 
+var trafficChannel = Channel.CreateBounded<LogEntry>(channelOptions);
+
+
+// Instanciando os Serviços (Injeção de Dependência Manual)
+var captureService = new PacketCaptureService(trafficChannel.Writer); // Produtor
+var analysisService = new AnalysisService(trafficChannel.Reader); // Consumidor
+
+
+
+CaptureDeviceList devices = CaptureDeviceList.Instance;
 ICaptureDevice device = devices.First(d => d.Name == nomeDaMinhaInterface);
 
-device.OnPacketArrival += anl.OnPacketArrival;
+device.OnPacketArrival += captureService.OnPacketArrival;
+
+// Iniciando a captura
 
 device.Open(DeviceModes.Promiscuous, 1000);
+Console.WriteLine($"\n-- SENTINELA INICIADO na interface: {nomeDaMinhaInterface} --");
+Console.WriteLine("Pressione [ENTER] para parar.\n");
+
+// Iniciando as Threads
+// Iniciamos a captura (Driver -> CaptureService -> Channel)
 device.StartCapture();
 
-Console.WriteLine("=== MONITORAMENTO ATIVO ===");
-Console.WriteLine("O Sentinela está ouvindo... Pressione ENTER para parar e fechar.");
+// Iniciamos a análise (Channel -> AnalysisService -> Console) em uma Task separada
+using var cts = new CancellationTokenSource();
+var analysisTask = Task.Run(() => analysisService.StartAnalysisLoopAsync(cts.Token));
 
-//  O BLOQUEIO (Essencial para o código não fechar sozinho)
-Console.ReadLine(); 
+// Mantém o programa rodando até o usuário dar Enter
+Console.ReadLine();
 
-//  PARADA LIMPA (Executa após o Enter)
+// 6. Encerramento Gracioso
+Console.WriteLine("Parando captura...");
 device.StopCapture();
 device.Close();
 
-Console.WriteLine("Monitoramento encerrado com segurança.");
+Console.WriteLine("Parando análise...");
+cts.Cancel(); // Avisa o loop do AnalysisService para parar
+await analysisTask; // Espera terminar
 
+Console.WriteLine("Sentinela encerrado.");
